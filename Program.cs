1413ef11fe71d2da49216819de66f07286e8e881
Andrey Rafael Silva <rafaelsilvaandrey@gmail.com>
2023-10-24 11:28:55 -0300

    Reajustes de formatacao

--- Program.cs
+++ Program.cs
@@ -10,20 +10,24 @@ namespace Calculadora
             Console.WriteLine("Calculadora");
 
             //Soma
+            Console.WriteLine("Soma");
             Soma soma = new Soma();
             Console.WriteLine(soma.Somar(2, 2));
 
             //Multiplicacao
+            Console.WriteLine("Multiplição");
             Multiplicacao multiplicacao = new Multiplicacao();
             Console.WriteLine(multiplicacao.Multiplicar(2,3));
 
             //Divisao
+            Console.WriteLine("Divisão");
             Divisao divisao = new Divisao();
             Console.WriteLine(divisao.Dividir(10, 2));
 
             //Subtracao
+            Console.WriteLine("Subtração");
             Subtracao subtracao = new Subtracao();
-            Console.WriteLine(subtracao.Subtrair(10, 2));
+            Console.WriteLine(subtracao.Subtrair(5, 2));
         }
     }
 }
